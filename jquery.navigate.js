88906f2946848ba514607ff1a4fcde093faa1f3d
Stuart Basden <stu@t.apio.ca>
2011-03-02 17:25:13 -0500

    hashchange included

--- jquery.navigate.js
+++ jquery.navigate.js
@@ -1,11 +1,22 @@
 /**
- * JavaScript created for Fiona Barratt Interiors
- * Created by Stuart Basden stu@t.apio.ca 2011
- * Released as Open Source under MIT license
+ * jQuery.navigate v 0.2 - 2nd March 2011
+ * navigate.apio.ca
  * 
+ * Copyright (c) 2010 Stuart Basden
+ * Released under MIT license
+ * https://github.com/jquery/jquery/blob/master/MIT-LICENSE.txt
+ * 
+ * ToDo: submenu entry, event creation, triple layer?
 **/
 
-
+// Console log debugger (remove)
+if (!("console" in window) || !("firebug" in console)) {
+	var names = ["log", "debug", "info", "warn", "error", "assert", "dir", "dirxml", "group", "groupEnd", "time", "timeEnd", "count", "trace", "profile", "profileEnd"];
+	window.console = {};
+	for (var i = 0, len = names.length; i < len; ++i) {
+		window.console[names[i]] = function(){};
+	}
+}
 
 (function( $ ){
   $.fn.navigate = function( options ) {
@@ -14,10 +25,13 @@
 			'mainCycle' : {
 				fx:'blindX',
 				timeout:0
-			},
+			}, // jQuery.cycle JSON settings. Remember timeout:0
 			'subCycle'  : {
-				fx:'blindY'
-			}
+				fx:'blindY',
+				timeout:0
+			}, // jQuery.cycle JSON settings. Remember timeout:0
+			'subpageContainer' : '.subpages', // Selector for container of subpages
+			'instantLoad' : 0 // Set to 1 for the main page to appear initially without cycling
 		};
 
 
@@ -28,86 +42,78 @@
 				$.extend( settings, options );
 			}
 			
-			var selector = $(this);
 			
-			var pageSlideNos = {};
+			settings.selector = $(this);
+			settings.pageSlideNos = {};
+			settings.subpageSlideNos = {};
+			
+			var home = '';
+			
 			var i = 0;
-			
 			// Get the pages 
 			$(this).children().each(function() {
 				var id = $(this).attr('id');
 				//console.log(id); // Will log all pages IDs
-				pageSlideNos[id] = i;
+				settings.pageSlideNos[id] = i;
+				if (i == 0) {home = id;}
 				i++;
 				
 				$(this).addClass('page');
 			});
-			//console.log(pageSlideNos); // Will log the JSON
-			
-			// Show the initial slide (change to entry hash)
-			window.location.hash = "#Home";
-			// CHANGE!!!
+			console.log(settings['pageSlideNos']); // Will log the JSON
 			
 			
 			// Start cycling the main pages
 			$(this).cycle(settings['mainCycle']);
 			
-			$('a').click(['selector','pageSlideNos'],function() {
+			
+			// Show the initial slide (change to entry hash)
+			if (window.location.hash == '') {
+				window.location.hash = '#'+home.charAt(0).toUpperCase() + home.slice(1);
+			}
+			var pages = {
+				'oldPage' : '',
+				'newPage' : window.location.hash.toLowerCase(),
+				'oldSubPage' : '',
+				'newSubPage' : ''
+			};
+			if (pages.newPage.indexOf('/') !=-1) {					
+				var hashArray = pages.newPage.split('/');
+				pages.newPage = hashArray[0];
+				pages.newSubPage = hashArray[1];
+				//console.log(pages.newPage, pages.newSubPage);
+			}
+			navigate(settings, pages);
+			
+			// Force main page to appear instantly and not cycle in (not sure why this works)
+			if (settings.instantLoad) {
+				navigate(settings, pages);
+			}		
+			
+			// Detect hash change and navigate
+			$(window).hashchange( function(){
+				pages = {
+					'oldPage' : pages.newPage,
+					'newPage' : window.location.hash.toLowerCase(),
+					'oldSubPage' : pages.newSubPage,
+					'newSubPage' : ''
+				};
+				if (pages.newPage.indexOf('/') !=-1) {					
+					var hashArray = pages.newPage.split('/');
+					pages.newPage = hashArray[0];
+					pages.newSubPage = hashArray[1];
+				}
+				//console.log(pages);
+				navigate(settings, pages);
+			});
+			
+			
+			$('a').click(settings,function() {
 				
 				if ($(this).attr('href').charAt(0) == '#') {
 					
 					// Set variables
 					var link = $(this).attr('href');
-					
-					// Begin main menu a
-					oldPage = window.location.hash.toLowerCase();
-					newPage = $(this).attr('href').toLowerCase();
-					oldSubPage = '', newSubPage = '';
-					
-					if (oldPage == newPage) {return false;}
-					//console.log(oldPage, newPage);
-					
-					//*
-					if (oldPage.indexOf('/') !=-1) {
-						var hashArray = oldPage.split('/');
-						oldPage = hashArray[0];
-						oldSubPage = hashArray[1];
-						//console.log(oldPage, oldSubPage);
-					}
-					if (newPage.indexOf('/') !=-1) {					
-						var hashArray = newPage.split('/');
-						newPage = hashArray[0];
-						newSubPage = hashArray[1];
-						//console.log(newPage, newSubPage);
-					}//*/
-					
-					if (oldSubPage == '' && newSubPage == '') {
-					// Going from main page to main page (#home -> #about)
-						console.log('main page to main page');
-						
-						mainToMain(selector, pageSlideNos, oldPage, newPage);
-						
-					} else if (oldSubPage == '' && newSubPage != '') {
-					// Going from main page to sub page(#home-> #gallery/page2 and #gallery-> #gallery/page2)
-						console.log('main page to sub page');
-						
-						mainToSub(selector, pageSlideNos, oldPage, newPage, newSubPage);
-						
-					} else if (oldSubPage != '' && newSubPage != '') {
-					// Going from sub page to sub page (#gallery/page2 -> #gallery/page3)
-						console.log('sub page to sub page');
-						
-						subToSub(selector, pageSlideNos, oldSubPage, newSubPage);
-						
-					} else if (oldSubPage != '' && newSubPage == '') {
-					// Going from sub page to main page (#gallery/page2 -> #gallery and #gallery/page2 -> #home)
-						console.log('sub page to main page');
-						
-						subToMain(selector, pageSlideNos, oldPage, oldSubPage, newPage)
-						
-					}
-					
-					
 					
 					
 					// Set the new location
@@ -128,141 +134,70 @@
 
 
 
-function mainToMain(selector, pageSlideNos, oldPage, newPage) {
-	var slide = pageSlideNos[newPage.replace('#','')];
-	//console.log(slide);
-	selector.cycle(slide);
-}
+function navigate(settings, pages) {
+	if (pages.oldPage != pages.newPage) {
+		console.log('main cycle required');
+		
+		// Go to page
+		var slide = settings.pageSlideNos[pages.newPage.replace('#','')];
+		settings.selector.cycle(slide);
 
+		// Create subpage cycle
+		if ($(pages.newPage).children(settings['subpageContainer']).length != 0) {
+			$(pages.newPage).children(settings['subpageContainer']).cycle(settings['subCycle']).addClass('cycling');
+			
+			// Get the subpages index
+			var j = 0;
+			$(pages.newPage).children(settings['subpageContainer']).children().each(function() {
+				var id = $(this).attr('id');
+				//console.log(id); // Will log all pages IDs
+				settings.subpageSlideNos[id] = j;
+				j++;
 
+				$(this).addClass('subpage');
+			});
+			//console.log(settings['subpageSlideNos']); // Will log the JSON
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/******************
-* Main Navigation *
-******************/
-
-function hidePage(oldPage, newPage, subPage) {
-	$('.active').removeClass('active');
-	if (oldPage == '#home') {
-		$('#logo').delay(900).animate({'left':'44px'},600, 'easeOutExpo');
-		$('#logo2').animate({'left':'-823px'},600, 'easeOutExpo');
-	}
-	
-	$(oldPage + ' .submenu').delay(300).animate({'left':'-150px'},'slow');
-	
-	if (subPage === '') {
-		subPage = oldPage + ' .mainContentPanel';
-		revertSubPage = 1;
+		}
+		
+		
+		// Go to subpage
+		if (pages.newSubPage != '') {
+			goToSubPage(settings, pages);
+		}
+		
+		
+		// Destroy oldpage subpage cycle
+		if ($(pages.oldPage).children(settings['subpageContainer']).length != 0) {
+			$(pages.oldPage+' '+settings['subpageContainer']).cycle('destroy');
+		}
+		
+		
+		
+		
 	} else {
-		subPage = '.contentPanel';
-		revertSubPage = 0;
+		console.log('no main cycle');
+		if (pages.newSubPage == '') {
+			// Go to first slide
+			$(pages.newPage).children(settings['subpageContainer']).cycle(0);
+		} else {
+			goToSubPage(settings, pages);
+		}
 	}
-	//console.log(subPage);
-	$(subPage).animate({'left':'-800px'},'slow', function() {
-		$(oldPage).hide();
-		if (revertSubPage == 1) {subPage = '';}
-		showMainPage(newPage, subPage);
-	});
+}
+
+function goToSubPage(settings, pages) {
+	var subslide = settings.subpageSlideNos[pages.newSubPage];
+	$(pages.newPage).children(settings['subpageContainer']).cycle(subslide);
 }
 
 
-
-function showMainPage(newPage, subPage) {
-	// Add .active to menu
-	var newHref = newPage.charAt(1).toUpperCase() + newPage.slice(2);
-	$('#menu a[href=#'+newHref+']').addClass('active');
-	
-	// Animate logo and menu opacity
-	if (newPage == '#home') {
-		$('#mainBG').animate({'opacity':'0.7'});
-		$('#logo').animate({'left':'-184px'},600, 'easeOutExpo');
-		$('#logo2').delay(1000).animate({'left':'155px'},600, 'easeOutExpo');
-	} else {
-		$('#mainBG').animate({'opacity':'1'});
-	}
-	
-	// Show new page and subpage
-	$(newPage).show(0,function() {
-		$(newPage + ' .submenu').delay(600).animate({'left':'368px'});
-		if (subPage === '') {subPage = newPage + ' .mainContentPanel';}
-		showSubPage(subPage);
-	});
-}
-
-/******************
-* Sub  Navigation *
-******************/
-
-
-
-function hideSubPage(oldPage, newPage) {
-	$('.activeSub').removeClass('activeSub');
-	$(oldPage).animate({'left':'-800px'},400, function() {
-		if(newPage) {showSubPage(newPage);}
-	});
-}
-
-function showSubPage(newSubPage) {
-	// Add .subActive to menu
-	var newHref = newSubPage.charAt(1).toUpperCase() + newSubPage.slice(2);
-	$('.submenu a[href="#Projects/'+newHref+'"]').addClass('activeSub');
-	
-	// Cycle background
-	var bgSlide = {
-		'none':0,
-		'profile':1,
-		'projects':2,
-		'no14':3,
-		'thelodge':4,
-		'apartmentchelsea':5,
-		'townhousechelsea':6,
-		'kensington':7,
-		'stjohnswood':8,
-		'swlondon':9,
-		'current':10,
-		'services':11,
-		'contact':12
-	};
-	var slide = newSubPage.replace('#','');
-	if (newSubPage.indexOf('mainContentPanel') !=-1) {
-		var whichMain = newSubPage.replace('#','').replace(' .mainContentPanel','');
-		slide=whichMain;
-		if (bgSlide[slide] == undefined) {slide='none';}
-	}
-	if (bgSlide[slide] == undefined) {slide='current';}
-	var newBG = bgSlide[slide];
-	//console.log(slide);
-	//$('#backgrounds').cycle(newBG);
-	
-	$(newSubPage).delay(400).animate({'left':'368px'});
-}
-//*/
-
-
-
-
-
-
-
+/*
+ * jQuery hashchange event - v1.3 - 7/21/2010
+ * http://benalman.com/projects/jquery-hashchange-plugin/
+ * 
+ * Copyright (c) 2010 "Cowboy" Ben Alman
+ * Dual licensed under the MIT and GPL licenses.
+ * http://benalman.com/about/license/
+ */
+(function($,e,b){var c="hashchange",h=document,f,g=$.event.special,i=h.documentMode,d="on"+c in e&&(i===b||i>7);function a(j){j=j||location.href;return"#"+j.replace(/^[^#]*#?(.*)$/,"$1")}$.fn[c]=function(j){return j?this.bind(c,j):this.trigger(c)};$.fn[c].delay=50;g[c]=$.extend(g[c],{setup:function(){if(d){return false}$(f.start)},teardown:function(){if(d){return false}$(f.stop)}});f=(function(){var j={},p,m=a(),k=function(q){return q},l=k,o=k;j.start=function(){p||n()};j.stop=function(){p&&clearTimeout(p);p=b};function n(){var r=a(),q=o(m);if(r!==m){l(m=r,q);$(e).trigger(c)}else{if(q!==m){location.href=location.href.replace(/#.*/,"")+q}}p=setTimeout(n,$.fn[c].delay)}$.browser.msie&&!d&&(function(){var q,r;j.start=function(){if(!q){r=$.fn[c].src;r=r&&r+a();q=$('<iframe tabindex="-1" title="empty"/>').hide().one("load",function(){r||l(a());n()}).attr("src",r||"javascript:0").insertAfter("body")[0].contentWindow;h.onpropertychange=function(){try{if(event.propertyName==="title"){q.document.title=h.title}}catch(s){}}}};j.stop=k;o=function(){return a(q.location.href)};l=function(v,s){var u=q.document,t=$.fn[c].domain;if(v!==s){u.title=h.title;u.open();t&&u.write('<script>document.domain="'+t+'"<\/script>');u.close();q.location.hash=v}}})();return j})()})(jQuery,this);
